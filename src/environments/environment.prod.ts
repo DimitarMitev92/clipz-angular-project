ad0c8e48fd0eba6b142c149227c889b93b43ecb4
Dimitar Mitev <d.mitev920123@gmail.com>
2023-09-06 12:12:02 +0300

    Integrate Firebase SDK

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC2xIglTS-qE1b8GQKlDahRZnbsy5V4KyI',
+    authDomain: 'clips-ee916.firebaseapp.com',
+    projectId: 'clips-ee916',
+    storageBucket: 'clips-ee916.appspot.com',
+    appId: '1:314964611891:web:b25add10ce6d7a538b2446',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyC2xIglTS-qE1b8GQKlDahRZnbsy5V4KyI',
+    authDomain: 'clips-ee916.firebaseapp.com',
+    projectId: 'clips-ee916',
+    storageBucket: 'clips-ee916.appspot.com',
+    messagingSenderId: '314964611891',
+    appId: '1:314964611891:web:b25add10ce6d7a538b2446',
+  },
 };
 
 /*
