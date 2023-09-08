5edae9c0b4fba92a1fdf75b9621d97b8ffaa0e44
Dimitar Mitev <d.mitev920123@gmail.com>
2023-09-08 13:29:00 +0300

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,9 @@
 module.exports = {
-  purge: {
-    content: [
-      './src/**/*.{html,ts}'
-    ]
-  },
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
   theme: {
     extend: {},
-  },
-  variants: {
-    extend: {
-      opacity: ['disabled'],
-      backgroundColor: ['disabled']
-    },
   },
   plugins: [],
 }
